77460efaea3917e8ac565634235c61447130e545
Dalton Morrel <daltonmorrel7&@gmail.com>
2021-04-18 14:24:16 -0400

    heroku deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://Dalton-Morrel:Rodman7723@cluster0.i3wvc.mongodb.net/mernshopping?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
